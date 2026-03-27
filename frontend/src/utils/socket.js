0052df99d887d01f491bcdeede5bf45c5a0a3a28
João Souza Bispo <js_bispo1@MacBook-Air-de-Joao.local>
2026-03-27 10:37:25 -0400

    socket on front corrected

--- frontend/src/utils/socket.js
+++ frontend/src/utils/socket.js
@@ -1,2 +1,2 @@
 import { io } from "socket.io-client";
-export const socket = io("http://localhost:3000/", { withCredentials: true, autoConnect: false });
+export const socket = import.meta.env.MODE === "development" ? io("http://localhost:3000/", { withCredentials: true, autoConnect: false }) : io({ withCredentials: true, autoConnect: false });
